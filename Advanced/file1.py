02871be5beab06c69388fd18aaf5b063ecffedbe
Syed Kumail Haider <86777702+Syedkumailhaider512@users.noreply.github.com>
2023-07-08 00:28:05 +0500

    Advanced Files Added

---
+++ Advanced/file1.py
@@ -0,0 +1,10 @@
+# Number Conversions (Binary, Octal, Hex)
+a = 46
+b = 0o106
+
+
+
+print(hex(a))
+print(oct(a))
+print(bin(a))
+print(b)
---
+++ Advanced/file2.py
@@ -0,0 +1,7 @@
+# Built-In Object Types
+number = 512
+string = 'Kumail'
+lis = ['name','number', 21]
+tup = ('name', 'number', 21)
+dictionary = {'name':string, 'number':number, 'age': 21}
+file = open('python.txt', 'r').read()
