742d2d0ed735bf9c74f10a5aaa96087328406b93
Syed Kumail Haider <86777702+Syedkumailhaider512@users.noreply.github.com>
2023-07-08 12:08:36 +0500

    Dictionary File 7 Added

---
+++ 5 Dictionaries/dict7.py
@@ -0,0 +1,11 @@
+dictionary = {}
+
+dictionary['Japan'] = 'Country'
+dictionary['Pakistan'] = 'Country'
+
+
+print(dictionary)
+
+print(dictionary.keys())
+print(dictionary.values())
+print(dictionary.items())
