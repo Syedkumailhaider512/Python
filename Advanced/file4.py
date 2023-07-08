b5768d143c9708cf09b2a17170171da937066a5c
Syed Kumail Haider <86777702+Syedkumailhaider512@users.noreply.github.com>
2023-07-08 12:09:07 +0500

    File 4 & 5 Added

---
+++ Advanced/file4.py
@@ -0,0 +1,5 @@
+# Direct Number Conversion
+
+print('{:06b}'.format(10))
+print('{:07x}'.format(8))
+print('{:2x}'.format(2))
---
+++ Advanced/file5.py
@@ -0,0 +1,5 @@
+# Direct Number Conversion with zfill
+
+print('{:06b}'.format(10).zfill(14))
+print('{:07x}'.format(8).zfill(14))
+print('{:2x}'.format(2).zfill(14))
